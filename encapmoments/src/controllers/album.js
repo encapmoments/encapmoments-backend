a921d3493696459446d52b96ea06c5c783bcb611
JungYeonWoong <71026614+JungYeonWoong@users.noreply.github.com>
2025-04-10 14:28:03 +0900

    Update album.js

--- encapmoments/src/controllers/album.js
+++ encapmoments/src/controllers/album.js
@@ -17,20 +17,27 @@ exports.getUserAlbums = async (req, res) => {
 };
 
 exports.getOneAlbum = async (req, res) => {
-  const albumId = parseInt(req.params.id);
+  const albumId = parseInt(req.params.album_id);
   const userId = req.user.id;
 
   try{
-    const oneAlbum = await prisma.album.findMany({
-      where: {id:userId, album_id:req.param[0]}
+    const oneAlbum = await prisma.album.findUnique({
+      where: {
+        id_album_id: {
+          id: userId,
+          album_id: albumId
+        }
+      }
     });
+
+    if (!album) {
+      return res.status(404).json({ message: '앨범을 찾을 수 없습니다.' });
+    }
+
     res.json(oneAlbum);
-  } catch(error) {
+  } 
+    catch(error) {
     console.error('앨범ID로 앨범 한개 가져오기 실패:', error);
     res.status(500).json({ message: '서버 에러' });
   }
 };
-
-
-
-
