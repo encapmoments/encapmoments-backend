dc9f7285256f2e7d62d4df7eef37e9ca94392d35
JungYeonWoong <dusdnd0209@naver.com>
2025-05-13 13:09:07 +0900

    Add files from encapmoments folder

---
+++ src/controllers/album.js
@@ -0,0 +1,47 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+exports.getUserAlbums = async (req, res) => {
+  const userId = req.user.id; // 토큰에서 가져온 user id
+
+  try {
+    const albums = await prisma.album.findMany({
+      where: { id: userId },
+      orderBy: { uploaded_at: 'desc' }
+    });
+    res.json(albums);
+  } catch (error) {
+    console.error('앨범 목록 가져오기 실패:', error);
+    res.status(500).json({ message: '서버 에러' });
+  }
+};
+
+exports.getOneAlbum = async (req, res) => {
+  const albumId = parseInt(req.params.album_id);
+  const userId = req.user.id;
+
+  try{
+    const oneAlbum = await prisma.album.findUnique({
+      where: {
+        id_album_id: {
+          id: userId,
+          album_id: albumId
+        }
+      }
+    });
+
+    if (!album) {
+      return res.status(404).json({ message: '앨범을 찾을 수 없습니다.' });
+    }
+
+    res.json(oneAlbum);
+  } 
+    catch(error) {
+    console.error('앨범ID로 앨범 한개 가져오기 실패:', error);
+    res.status(500).json({ message: '서버 에러' });
+  }
+};
+
+
+
+
---
+++ src/routes/album.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const albumController = require('../controllers/album');
+
+// GET /album - 현재 로그인한 사용자의 앨범 목록 조회
+router.get('/', albumController.getUserAlbums);
+
+// GET /album_id에 해당하는 앨범 1개 조회회
+router.get('/:album_id', albumController.getOneAlbum);
+
+module.exports = router;
