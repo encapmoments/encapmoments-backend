106aab37b893f5157e3c5a4068f55ba0ed12f165
JungYeonWoong <71026614+JungYeonWoong@users.noreply.github.com>
2025-08-25 18:20:14 +0900

    Add files via upload

--- src/controllers/marketController.js
+++ src/controllers/marketController.js
@@ -6,34 +6,70 @@ const prisma = new PrismaClient();
 // DATABASE_URL(예: MySQL, PostgreSQL) 환경변수를 읽어 DB에 연결
 
 
-const moment = require("moment-timezone");
-const { completeMission } = require("../services/missionService");
-const { uploadToS3 } = require("../services/s3Service");
 
-
-
-
-// 앨범 목록 조회
-exports.getUserAlbums = async (req, res) => {
-  const userId = req.user.id; // 토큰에서 가져온 user id
-
-  try {
-    const albums = await prisma.album.findMany({
-      where: { id: userId },
-      orderBy: { created_at: 'desc' },
-      select: {
-        album_id: true,
-        album_title: true,
-        album_tag: true,
-        album_image: true,
-        location: true
+// 컨트롤러 함수 
+// Item 목록 조회
+exports.getItems = async (req, res) => {
+  
+  try{
+    const items = await prisma.reward_item.findMany({
+      orderBy:{
+        name: 'asc'   // 이름 오름차순 
+      },
+      select:{
+        item_id: true,
+        name: true,
+        description: true,
+        image_url: true,
+        category: true,
+        cost: true,
+        stock: true
       }
     });
-    console.log(`[GET] /album - userId: ${userId} -> ${albums.length}개 반환됨`);
-    res.status(200).json(albums);
-    
+
+    console.log('[GET] /itmes -> ${items.length}개 반환됨');
+    return res.status(200).json({message: '아이템 목록 조회 성공', items});
+
   } catch (error) {
-    console.error('앨범 목록 가져오기 실패:', error);
-    res.status(500).json({ message: '서버 에러' });
+    console.log('아이템 목록 가져오기 실패:', error);
+    return res.status(500).json({message: '서버 에러'});
+  }
+};
+
+// Item 상세 조회 
+exports.getOneItem = async (req, res) => {
+  const itemId = parseINT(req.params.item_id);
+
+  if (isNaN(itemId)) {
+    return res.status(400).json({ message: '잘못된 item_id 형식입니다. '});
+  }
+
+  try{
+    const oneItem = await prisma.reward_item.findUnique({
+      where: {
+        item_id: itemId
+      },
+      select: {
+        item_id: true,
+        name: true,
+        description: true,
+        image_url: true,
+        category: true,
+        cost: true,
+        stock: true,
+        created: true
+      }
+    });
+
+    if (!oneItem) {
+      return res.status(404).json({ message: '앨범 없음.'});
+    }
+
+    console.log('[GET] /items/:item_id 가져오기 성공');
+    return res.status(200).json({message: '아이템 상세 조회 성공', data: oneItem});
+
+  } catch (error) {
+    console.error('앨범 상세 조회 실패', error);
+    return res.status(500).json({ message: '서버 에러' });
   }
 };
