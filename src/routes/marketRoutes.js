871877edc47dc8266262e4d6fbf07949a6e79ed4
JungYeonWoong <71026614+JungYeonWoong@users.noreply.github.com>
2025-08-27 18:25:22 +0900

    Update marketRoutes.js

--- src/routes/marketRoutes.js
+++ src/routes/marketRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const marketController = require("../controllers/marketController");
- const verifyToken = require("../middlewares/authMiddleware");
+const verifyToken = require("../middlewares/authMiddleware");
 
 router.get("/items", marketController.getItems);
 router.get("/items/:item_id",marketController.getOneItem);
@@ -11,4 +11,5 @@ router.get("/me", verifyToken, marketController.getMyPurchases);
 router.patch("/use/:user_reward_id", verifyToken, marketController.useGifticon);
 
 module.exports = router;
+
 
