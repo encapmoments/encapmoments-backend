7b0858886885b79cd2390a395ab6d824f14560fc
JungYeonWoong <71026614+JungYeonWoong@users.noreply.github.com>
2025-08-27 18:23:52 +0900

    Add files via upload

--- src/routes/marketRoutes.js
+++ src/routes/marketRoutes.js
@@ -2,8 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const marketController = require("../controllers/marketController");
-// const upload = require("../middlewares/upload");
-// const verifyToken = require("../middlewares/authMiddleware");
+ const verifyToken = require("../middlewares/authMiddleware");
 
 router.get("/items", marketController.getItems);
 router.get("/items/:item_id",marketController.getOneItem);
